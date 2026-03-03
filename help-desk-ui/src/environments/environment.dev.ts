12321cb30c2efbfe71e52dd8164f6b351010f3b2
Nigel Arugay <nigel.arugay@endeavor-ms.com>
2026-03-04 00:56:12 +0800

    Working baseline snapshot

--- help-desk-ui/src/environments/environment.dev.ts
+++ help-desk-ui/src/environments/environment.dev.ts
@@ -5,5 +5,6 @@ export const environment = {
   //Azure Config
   azureClientId: '77dd2c9c-5d15-46ad-98d9-039c62d8ef9a',
   azureAuthority: 'https://login.microsoftonline.com/119de762-6e78-4af0-a159-76b9a12af1a4',
-  azureRedirectUri: 'https://i90.dev.vertek.com/help-desk/'
+  azureRedirectUri: 'https://i90.dev.vertek.com/help-desk/',
+  API_BASE_URL: "http://localhost:8080/qto"
 };
--- qto/qto-app/src/main/java/com/endeavorms/velocity/qto/WebMvcConfig.java
+++ qto/qto-app/src/main/java/com/endeavorms/velocity/qto/WebMvcConfig.java
@@ -1,17 +1,26 @@
 package com.endeavorms.velocity.qto;
 
-import com.endeavorms.velocity.qto.common.ResourceWrapperExcelHttpMessageConverter;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
+import com.endeavorms.velocity.qto.common.ResourceWrapperExcelHttpMessageConverter;
 
 /**
  * Web MVC configuration for custom message converters.
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
